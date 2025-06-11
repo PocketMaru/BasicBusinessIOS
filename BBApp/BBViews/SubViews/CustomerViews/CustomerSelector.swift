23eace7b34bf2a3e31f81e808562eb905de23c66
Joshua Hauer <joshuahauer@icloud.com>
2025-06-11 04:11:46 -0400

    Memory Leak Fix/App Name
    Fixed a memory leak from views being built with placeholders, replaced Text Fields with EmptyView().
    Changed the display name for the application to Basic Business.

--- BBApp/BBViews/SubViews/CustomerViews/CustomerSelector.swift
+++ BBApp/BBViews/SubViews/CustomerViews/CustomerSelector.swift
@@ -9,6 +9,6 @@ import SwiftUI
 
 struct CustomerSelector: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        EmptyView()
     }
 }
--- BBApp/BBViews/SubViews/InvoiceViews/InvoiceSummaryView.swift
+++ BBApp/BBViews/SubViews/InvoiceViews/InvoiceSummaryView.swift
@@ -9,6 +9,6 @@ import SwiftUI
 
 struct InvoiceSummaryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        EmptyView()
     }
 }
